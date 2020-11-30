eb23675610ece9baf043968845bfe4b628c1665a
wooshcz <wooshcz@gmail.com>
2020-11-30 14:35:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Bash Utils for working with the Akamai CDN
+
+## staging-cli.sh
+Bash script that is able to extract the currently used Edge Servers IP addresses for Akamai-hosted domains and apply these IPs into the /etc/hosts file to route the local traffic to Akamai Staging network. This is useful to test changes on Akamai Staging before deploying to production.
+
+### Usage
+
+   $ ./staging-cli.sh
+   Usage: ./staging-cli.sh [ apply | build | init | clean | reset ]
+
+* apply - applies the already built IP-Host entries into the main /etc/hosts. This needs to be run as sudo.
+* build - this takes the configuration file with the hostnames and builds the IP-Host mapping. This needs to be run first before the apply action can be used.
+* init - initialization of the tool. This takes a backup of the default values in /etc/hosts and stores them locally for reference.
+* clean - clears the built IP-Host mapping from disk -- ./hosts.staging
+* reset - reverts the /etc/hosts into the default state. This needs to be run as sudo -- sudo cp ./hosts.default /etc/hosts
+
+### Typical examples
+
+   $ ./staging-cli.sh init # initialize the tool
+   $ vim ./staging-hostnames-list.txt # edit the list hostnames manually
+   $ ./staging-cli.sh build # build the IP-Host mapping
+   $ sudo ./staging-cli.sh apply # apply the built mapping into /etc/hosts
+   $ sudo ./staging-cli.sh reset # revert the /etc/hosts into the default state
+   $ ./staging-cli.sh clean # clear the built IP-Host mapping from disk
+
+
---
+++ staging-cli/staging-cli.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+BASE_PATH=`dirname "$0"`
+HOSTNAMES_LIST_FILE=${BASE_PATH}/staging-hostnames-list.txt
+HOSTNAMES_LIST=`grep -v -E "^#(.*)" ${HOSTNAMES_LIST_FILE}`
+BUILD_HOSTS_FILE=${BASE_PATH}/hosts.staging
+DEFAULT_HOSTS_FILE=${BASE_PATH}/hosts.default
+
+# Print a usage message.
+usage() {
+  echo "Usage: $0 [ apply | build | init | clean | reset ]" 1>&2
+}
+
+if [[ ${BASH_ARGC} -ne 1 ]]; then
+  usage
+  exit 1
+fi
+
+apply() {
+  echo "Applying the built configuration ..."
+  if [[ -f ${BUILD_HOSTS_FILE} ]]; then
+    cat ${BUILD_HOSTS_FILE} >> /etc/hosts
+  else
+    echo "No configuration has been built yet, you might want to run '$0 build' first"
+    exit 1
+  fi
+}
+
+init() {
+  echo "Initializing the staging-cli, saving the default hosts file ..."
+  cat /etc/hosts > ${DEFAULT_HOSTS_FILE}
+  echo -n "" > ${BUILD_HOSTS_FILE}
+}
+
+reset() {
+  echo "Restoring the hosts file to the default state ..."
+  if [[ -f ${DEFAULT_HOSTS_FILE} ]]; then
+    cat ${DEFAULT_HOSTS_FILE} > /etc/hosts
+  else
+    echo "Default hosts file has not been saved yet. Please run '$0 init' first"
+    exit 1
+  fi
+}
+
+clean() {
+  echo "Clearing the built configuration ..."
+  echo -n "" > ${BUILD_HOSTS_FILE}
+}
+
+build() {
+  echo "Building the configuration ..."
+  CNTR=0
+  ARR_LEN=`wc -w <<< "${HOSTNAMES_LIST}"`
+  echo "Found ${ARR_LEN} hostnames in the list"
+  for hostname in ${HOSTNAMES_LIST}; do
+    ((CNTR++))
+    echo -n "${CNTR}/${ARR_LEN} | Processing ${hostname}"
+    hostname=`echo ${hostname} | tr -d '[:space:]'`
+    DIG_CNAME=`dig ${hostname} +short | grep -E "\.(edgekey|edgesuite)\.net\.$"`;
+    if [[ ${#DIG_CNAME} -gt 0 ]]; then
+      echo -n -e " | is hosted on Akamai"
+      STAGING_CNAME=`echo ${DIG_CNAME} | sed -E 's/(edgekey|edgesuite)\.net/\1-staging\.net/'`;
+      STAGING_IP_OUT=`dig ${STAGING_CNAME} +short | tail -1`;
+      echo -n -e " | staging IP address: ${STAGING_IP_OUT}"
+      echo -e "${STAGING_IP_OUT}\t${hostname}" >> ${BUILD_HOSTS_FILE}
+    else
+      echo -n -e " | skipping"
+    fi
+    echo -e " | done"
+  done
+}
+
+if [[ "${BASH_ARGV}" == "apply" ]]; then
+  reset
+  apply
+fi
+
+if [[ "${BASH_ARGV}" == "clean" ]]; then
+  clean
+fi
+
+if [[ "${BASH_ARGV}" == "init" ]]; then
+  init
+fi
+
+if [[ "${BASH_ARGV}" == "build" ]]; then
+  clean
+  build
+fi
+
+if [[ "${BASH_ARGV}" == "reset" ]]; then
+  reset
+fi
